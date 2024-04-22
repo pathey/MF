550280c594277e915456bfa65c5091ccd5ed25d1
pathey <peter@athey.org>
2024-04-21 22:32:30 -0700

    Big update, restricted refresh, movement, collision, etc.

--- Assembly_MF/MF.asm
+++ Assembly_MF/MF.asm
@@ -1,6 +1,10 @@
 #Author:	Peter Athey
 #Description: 	MF - MazeFighter: A rudimentary 3D rendering program
 
+# Some command prompts I used to run this on my machine
+# java -jar /c/Users/Peter/Downloads/Mars4_5.jar MF.asm
+
+
 .data
 player:			.asciiz "0"		#This is the player symbol on the overhead map
 newline:		.asciiz  "\n"
@@ -28,13 +32,90 @@ map:				#This defines a map for the player to navigate
 		
 #clear_screen:   	.asciiz "\033[2J"      # Escape sequence to clear the screen
 clear_screen:   	.byte 0x1b 0x5b 0x32 0x4a 0x00
-#clear_screen:   	.asciiz "Hello"      # Escape sequence to clear the screen
 #cursor_home:    	.asciiz "\033[20A"     # Escape sequence to move cursor to home (top-left corner)
 cursor_home:    	.byte 0x1b 0x5b 0x32 0x31 0x41 0x00
 cursor_down:    	.asciiz "\033[B"       # Escape sequence to move cursor down
 
+bonk:			.asciiz "\nBONK!\n"		#triggered by colliding with a wall.
+
 
 .text 				#Code starts here
+
+.macro storetvalues
+addi $sp, $sp, -4
+sw $t0, ($sp)
+addi $sp, $sp, -4
+sw $t1, ($sp)
+addi $sp, $sp, -4
+sw $t2, ($sp)
+addi $sp, $sp, -4
+sw $t3, ($sp)
+addi $sp, $sp, -4
+sw $t4, ($sp)
+addi $sp, $sp, -4
+sw $t5, ($sp)
+addi $sp, $sp, -4
+sw $t6, ($sp)
+addi $sp, $sp, -4
+sw $t7, ($sp)
+.end_macro
+
+.macro restoretvalues
+lw $t7, ($sp)
+addi $sp, $sp, 4
+lw $t6, ($sp)
+addi $sp, $sp, 4
+lw $t5, ($sp)
+addi $sp, $sp, 4
+lw $t4, ($sp)
+addi $sp, $sp, 4
+lw $t3, ($sp)
+addi $sp, $sp, 4
+lw $t2, ($sp)
+addi $sp, $sp, 4
+lw $t1, ($sp)
+addi $sp, $sp, 4
+lw $t0, ($sp)
+addi $sp, $sp, 4
+.end_macro
+
+.macro storesvalues
+addi $sp, $sp, -4
+sw $s0, ($sp)
+addi $sp, $sp, -4
+sw $s1, ($sp)
+addi $sp, $sp, -4
+sw $s2, ($sp)
+addi $sp, $sp, -4
+sw $s3, ($sp)
+addi $sp, $sp, -4
+sw $s4, ($sp)
+addi $sp, $sp, -4
+sw $s5, ($sp)
+addi $sp, $sp, -4
+sw $s6, ($sp)
+addi $sp, $sp, -4
+sw $s7, ($sp)
+.end_macro
+
+.macro restoresvalues
+lw $s7, ($sp)
+addi $sp, $sp, 4
+lw $s6, ($sp)
+addi $sp, $sp, 4
+lw $s5, ($sp)
+addi $sp, $sp, 4
+lw $s4, ($sp)
+addi $sp, $sp, 4
+lw $s3, ($sp)
+addi $sp, $sp, 4
+lw $s2, ($sp)
+addi $sp, $sp, 4
+lw $s1, ($sp)
+addi $sp, $sp, 4
+lw $s0, ($sp)
+addi $sp, $sp, 4
+.end_macro
 
 
 #gp +4 is the x-coordinate
@@ -42,36 +123,42 @@ cursor_down:    	.asciiz "\033[B"       # Escape sequence to move cursor down
 #gp +12 is the pickup type
 #gp +16 is the pickup availability (on cooldown or not)
 #gp +20 is the number of characters in a row
-
+#gp +24 is the previous x coordinate
+#gp +28 is the previous y coordinate
+#gp +32 is the previous pickup type
+#gp +36 is the previous availability
 
 main:
 	
 	li $t0, 9
 	sw $t0, 4($gp)			#Loads and stores x and y coordinates
+	sw $t0, 24($gp)
 	li $t1, 1
 	sw $t1, 8($gp)
+	sw $t1, 28($gp)
 	li $t2, 1
 	sw $t2, 12($gp)
+	sw $t2, 32($gp)
 	li $t3, 0
 	sw $t3, 16($gp)
+	sw $t3, 36($gp)
 	li $t4, 22
 	sw $t4, 20($gp)
 	
+	
 	li $v0, 4
-  la $a0, clear_screen
-  syscall
+  	la $a0, clear_screen
+  	syscall
 
-#j end
+	#j end
 
 	jal draw	         	# Jump to the printMap subroutine
 	
-  #li $v0, 4
-  #la $a0, clear_screen
-  #syscall
-	
-	jal draw
-	
-	j end
+  	#li $v0, 4
+ 	#la $a0, clear_screen
+  	#syscall
+	#jal draw
+	#j end
 	
 while:
 	
@@ -87,17 +174,17 @@ while:
 	addi $sp, $sp, 4
 	
 	beq $t4, 0, skipDraw
-    	jal draw           	# Jump to the printMap subroutine
+    	jal draw           		# Jump to the printMap subroutine
     	skipDraw:
     	
-    	li $v0, 11			#Checking for user input/jumping appropriately
+    	li $v0, 12			#Checking for user input/jumping appropriately
     	syscall
     	
     	move $a0, $v0			#storing v0 in a0 for convention matching
     	
     	beq $a0, 0x71, end		#Jump to end if input character is q
 
-	bne $a0, -1, noInputHandling	#Handle input if it isn't -x
+	beq $a0, -1, noInputHandling	#Handle input if it isn't -1
 	jal inputHandler
 	noInputHandling:
 
@@ -107,8 +194,18 @@ while:
 
 #Check if there is a need to re-render
 checkChange:
-
-	lw $t4, 4($gp)
+	
+	storetvalues
+	
+	#MUST REBUILD TO PROPERLY HANDLE CANON/NONCANON POSITIONS (STORE IN ANOTHER GP POSITION, THEN CHECK)
+	
+	lw $t0, 24($gp)			#Grab the previous values
+	lw $t1, 28($gp)
+	lw $t2, 32($gp)
+	lw $t3, 36($gp)
+	
+	
+	lw $t4, 4($gp)			#Grab the currently stored values
 	lw $t5, 8($gp)
 	lw $t6, 12($gp)
 	lw $t7, 16($gp)
@@ -120,60 +217,53 @@ checkChange:
 	
 	li $v0, 0
 	
+	restoretvalues
+	
 	jr $ra
 	
 	changeTrue:
 	li $v0, 1
-	lw $t0, 4($gp)
-	lw $t1, 8($gp)
-	lw $t2, 12($gp)
-	lw $t3, 16($gp)
 	
+	restoretvalues
 	
 	jr $ra
 	
 	
 inputHandler:
-	addi $sp, $sp, -4
-	sw $t0, ($sp)
-	addi $sp, $sp, -4
-	sw $t1, ($sp)
-	addi $sp, $sp, -4
-	sw $t2, ($sp)
-	addi $sp, $sp, -4
-	sw $t3, ($sp)
-	addi $sp, $sp, -4
-	sw $t4, ($sp)
-	addi $sp, $sp, -4
-	sw $t5, ($sp)
-	addi $sp, $sp, -4
-	sw $t6, ($sp)
+	storetvalues
+	storesvalues
 	
-	lw $t3, 4($gp)			#Store x and y coordinates in temporary registers for checking after
-	lw $t4, 8($gp)
+	lw $t1, 4($gp)			#Store x and y coordinates for current access
+	lw $t2, 8($gp)
+	
+	
+	
+	
+	lw $t5, 4($gp)			#Store x and y coordinates in temporary registers for checking after
+	lw $t6, 8($gp)
 	
 	move $t0, $a0
 	
 	bne $t0, 0x77, noForward
-	addi $t4, $t4, -1
+	addi $t2, $t2, -1
 	noForward:
 	bne $t0, 0x73, noBackward
-	addi $t4, $t4, 1
+	addi $t2, $t2, 1
 	noBackward:
 	bne $t0, 0x61, noLeft
-	addi $t3, $t3, -1
+	addi $t1, $t1, -1
 	noLeft:
 	bne $t0, 0x64, noRight
-	addi $t3, $t3, 1
+	addi $t1, $t1, 1
 	noRight:
 	
 	lw $t6, 20($gp)
 	
-	la $a0, map            	# Load the address of the map into $a0
+	la $a0, map            		# Load the address of the map into $a0
     	li $v0, 4               	# System call code for printing a string
-       	mul $t1, $t4, $t6
-       	add $a0, $a0, $t1
+       	mul $t3, $t2, $t6
        	add $a0, $a0, $t3
+       	add $a0, $a0, $t1
 
        	li $s1, 4				#divisor
 			
@@ -184,33 +274,27 @@ inputHandler:
 		
 	mul $t0, $t0, 4
 		
-	lw $t1, ($t0)
+	lw $t3, 0($t0)
 	mul $s2, $s2, 8
-	srlv $t1, $t1, $s2
-	and $t1, 0xff
-	beq $t1, 0x23, checkFail
-	sw $t3, 4($gp)
-       	sw $t4, 8($gp)
-        
-        checkFail:
-        li $v0, -1
+	srlv $t3, $t3, $s2
+	and $t3, 0xff
+	beq $t3, 0x23, cantMove
+	sw $t1, 4($gp)
+       	sw $t2, 8($gp)
+       	j canMove
+	cantMove:
+	li $v0, 4           	# System call code for printing a string
+        la $a0, bonk     	# Load the newline string address
+        syscall
+	canMove:
+	
+	
+	
+	restoresvalues
+	restoretvalues
+	
 	
 	jr $ra
-	
-	lw $t6, ($sp)
-	addi $sp, $sp, 4
-	lw $t5, ($sp)
-	addi $sp, $sp, 4
-	lw $t4, ($sp)
-	addi $sp, $sp, 4
-	lw $t3, ($sp)
-	addi $sp, $sp, 4
-	lw $t2, ($sp)
-	addi $sp, $sp, 4
-	lw $t1, ($sp)
-	addi $sp, $sp, 4
-	lw $t0, ($sp)
-	addi $sp, $sp, 4
 
 #ClearScreen Subroutine
 clearScreen:
@@ -239,22 +323,7 @@ draw:
 
 	addi $sp, $sp, -4		#Preserves space for current function
 	sw $a0, 0($sp)
-	addi $sp, $sp, -4
-	sw $t0, 0($sp)
-	addi $sp, $sp, -4
-	sw $t1, 0($sp)
-	addi $sp, $sp, -4
-	sw $t2, 0($sp)
-	addi $sp, $sp, -4
-	sw $t3, 0($sp)
-	addi $sp, $sp, -4
-	sw $t4, 0($sp)
-	addi $sp, $sp, -4
-	sw $t5, 0($sp)
-	addi $sp, $sp, -4
-	sw $t6, 0($sp)
-	addi $sp, $sp, -4
-	sw $t7, 0($sp)
+	storetvalues
 	addi $sp, $sp, -4
 	sw $s0, 0($sp)
 	addi $sp, $sp, -4
@@ -278,16 +347,16 @@ draw:
 	#lw $ra, ($sp)
 	#addi $sp, $sp, 4
 	
-	lw $t1, 4($gp)			# Load x-coordinate into t1
-	lw $t2, 8($gp)			# Load y-coordinate into t2
+	lw $t1, 4($gp)				# Load x-coordinate into t1
+	lw $t2, 8($gp)				# Load y-coordinate into t2
 	li $t3, 0
 	li $t6, 0x51
 	
 	
-	la $a0, map            	# Load the address of the map into $a0
-    	li $v0, 4               	# System call code for printing a string
+	la $a0, map         	 			  	# Load the address of the map into $a0
+    	li $v0, 4               				# System call code for printing a string
     	loop:
-        	lb $a1, 0($a0)      	# Load a string from the map
+        	lb $a1, 0($a0) 				     	# Load a string from the map
         	bne $t2, $t3, postPrintMapPlayerCheck		#If the player y coordinate doesn't match the current row, jump past the x coord check
         	li $t4, 0
         	playerInsert:
@@ -327,8 +396,9 @@ draw:
         	li $v0, 4           	# System call code for printing a string
         	la $a0, newline     	# Load the newline string address
         	syscall             	# Print a newline
-        	jr $ra              	# Return from the subroutine
-	
+        	
+	sw $t1, 24($gp)
+	sw $t2, 28($gp)
 	
 	lw $s3, 0($sp)
         addi $sp, $sp, 4
@@ -338,24 +408,12 @@ draw:
         addi $sp, $sp, 4
         lw $s0, 0($sp)
         addi $sp, $sp, 4
-	lw $t7, 0($sp)
-        addi $sp, $sp, 4    
-        lw $t6, 0($sp)
-        addi $sp, $sp, 4        
-        lw $t5, 0($sp)
-        addi $sp, $sp, 4
-        lw $t4, 0($sp)
-        addi $sp, $sp, 4
-        lw $t3, 0($sp)
-        addi $sp, $sp, 4
-        lw $t2, 0($sp)
-        addi $sp, $sp, 4
-        lw $t1, 0($sp)
-        addi $sp, $sp, 4
-        lw $t0, 0($sp)
-        addi $sp, $sp, 4
+	restoretvalues
         lw $a0, 0($sp)
         addi $sp, $sp, 4		#Restores saved space
+	
+	jr $ra            	  	# Return from the subroutine
+
 
 end:
     # End of the program
